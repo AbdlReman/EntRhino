852337eb91d2f4fbf69ebcac694e377480203760
CoderMasterypro <abdulrehmanabbasi5067794@gmail.com>
2024-09-12 07:19:58 -0700

    title

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       {loader && <PreLoader />}
       <Head>
-        <title>Seeva - Medical and Dental React Nextjs Template</title>
+        <title>ENT And Rhinoplasty Hub</title>
         <link
           rel="shortcut icon"
           href="assets/img/favicon.ico"
